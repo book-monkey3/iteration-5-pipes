69466385bce2e39164033e7bf5fc0c5c779d7ee3
The Buildbot <buildbot@angular2buch.de>
2018-12-10 23:01:57 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/2a5bb7db573f2432d3b32d9b330a22dc3e83a141
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/183

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DateValueAccessorModule } from 'angular-date-value-accessor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 import { SearchComponent } from './search/search.component';
 import { TokenInterceptor } from './shared/token-interceptor';
+import { BookFormComponent } from './book-form/book-form.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { FormMessagesComponent } from './form-messages/form-messages.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { IsbnPipe } from './shared/isbn.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent,
+    SearchComponent,
+    BookFormComponent,
+    CreateBookComponent,
+    FormMessagesComponent,
+    EditBookComponent,
+    IsbnPipe
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    DateValueAccessorModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
--- src/app/book-list-item/book-list-item.component.ts
+++ src/app/book-list-item/book-list-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 
-import { Book } from '../../shared/book';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'bm-book-list-item',
